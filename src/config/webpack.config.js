4a079512030b2f32998fa40705849ea648147e2a
Johnny Estrada <johnnye562@yahoo.com>
2023-01-17 15:43:08 -0800

    Create and configure wepack config file to use aliases

---
+++ src/config/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    resolve: {
+      extensions: ['js', 'ts'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        // ...etc
+      },
+    },
+  }
