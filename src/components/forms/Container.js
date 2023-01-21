35f41a09391830ad4a42987f513e6b063dad6d3f
Johnny Estrada <johnnye562@yahoo.com>
2023-01-20 18:29:24 -0800

    Create Container component and styles

---
+++ src/components/forms/Container.js
@@ -0,0 +1,12 @@
+import Input from "./Input";
+import './Container.css'
+
+function Container() {
+  return (
+    <section className="input-wrapper">
+      <Input />
+    </section>
+  );
+}
+
+export default Container;
