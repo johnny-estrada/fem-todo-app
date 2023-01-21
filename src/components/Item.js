76a144bb270647e0dd3b20d418e325a3c93a6ab1
Johnny Estrada <johnnye562@yahoo.com>
2023-01-20 18:26:30 -0800

    Create Item component and style sheet

---
+++ src/components/Item.js
@@ -0,0 +1,13 @@
+import "./Item.css";
+
+function Item() {
+  return (
+    <section className="item-container">
+      <input type="checkbox" id="" name=""></input>
+      <span className="checkbox"></span>
+      <p className="text">Return Amazon orders to UPS store</p>
+    </section>
+  );
+}
+
+export default Item;
